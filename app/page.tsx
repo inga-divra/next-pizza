e8c4123f2c319196ce0203e5831f404b61cd7911
inga-divra <ingady@gmail.com>
2025-02-01 21:40:32 +0200

    Updated global CSS file with ShadCN UI and Tailwind configurations.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className='m-8'>
+      <h1 className='mb-5'>Home</h1>
+      <Button>Cart</Button>
+    </div>
+  );
 }
