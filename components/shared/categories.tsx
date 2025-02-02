12d9d9b49fbd7230c7b3bb353ef8d8bbe7d384a5
inga-divra <ingady@gmail.com>
2025-02-02 18:30:48 +0200

    Added sorting popup component

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -18,7 +18,7 @@ export const Categories: React.FC<Props> = ({ className }) => {
           className={cn(
             'flex items-center font-bold h-11 rounded-2xl px-5',
             activeIndex === index &&
-              'bg-white shadow-md shadow-gray-200 text-primary'
+              'bg-white shadow-md shadow-gray-50 text-primary'
           )}
           key={index}
         >
---
+++ components/shared/sort-popup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface Props {
+  className?: string;
+}
+export const SortPopup: React.FC<Props> = ({ className }) => {
+  return <div className={className}></div>;
+};
