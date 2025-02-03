2c96ac8d4476c5c5da08266127fef8eae2516b32
inga-divra <ingady@gmail.com>
2025-02-03 18:05:11 +0200

    Added price filtering and improved screen reader accessibility
    - Introduced price range filtering with min and max price inputs.
    - Enhanced accessibility by using <fieldset>, <legend>, and aria-label for screen readers.
    - Added a descriptive label for the price range input fields.
    - Updated the Title component to include an aria-label for improved screen reading.

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui';
 
 interface Props {
   className?: string;
@@ -8,12 +9,49 @@ interface Props {
 export const Filters: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
-      <Title text='Filter' size='sm' className='mb-5 font-bold' />
+      <Title
+        text='Filter'
+        size='sm'
+        className='mb-5 font-bold'
+        aria-label='Filters section'
+      />
 
-      <div className='flex flex-col gap-4'>
+      {/* PRODUCT TYPE FILTERING */}
+      <fieldset className='flex flex-col gap-4'>
+        <legend className='sr-only'>Product Type Filter</legend>{' '}
+        {/* Hidden heading for accessibility */}
         <FilterCheckbox text='Customizable' value='1' />
         <FilterCheckbox text='New Arrivals' value='2' />
+      </fieldset>
+
+      {/* PRICE FILTERING */}
+      <div className='mt-5 border-y border-y-neutral-100 py-6 pb-7'>
+        <label className='font-bold mb-3 block' htmlFor='price-range'>
+          Price range:
+        </label>
+        <div className='flex gap-3 mb-5' id='price-range'>
+          <Input
+            type='number'
+            placeholder='Min'
+            min={0}
+            max={1000}
+            defaultValue={0}
+            aria-label='Minimum price'
+          />
+          <span className='self-center font-semibold'>to</span>
+          <Input
+            type='number'
+            placeholder='Max'
+            min={0}
+            max={1000}
+            defaultValue={300000}
+            aria-label='Maximum price'
+          />
+        </div>
       </div>
+
+      {/* Ingredients filtering */}
+      {/* Dough type filtering */}
     </div>
   );
 };
