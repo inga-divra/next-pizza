89c8a77ac1b84da999f4935825ced626fe1d5bed
inga-divra <ingady@gmail.com>
2025-02-05 10:28:21 +0200

    Created ProductsGroupList component for rendering grouped product lists .
    - Added `ProductsGroupList` component to display products in categories
    - Defined `Product` and `ProductVariant` interfaces for better type safety
    - Ensured `price` is taken from the first variant in `items`
    - Used `cn` utility for flexible class management

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,13 +1,48 @@
 'use client';
 import React from 'react';
+import { Title } from './title';
+import { cn } from '@/lib/utils';
+import { ProductCard } from './product-card';
+
+interface ProductVariant {
+  price: number;
+}
+
+interface Product {
+  id: number;
+  name: string;
+  imageUrl: string;
+  items: ProductVariant[];
+}
 
 interface Props {
   title: string;
-  items: any[];
+  items: Product[];
   categoryId: number;
   className?: string;
   listClassName?: string;
 }
-export const ProductsGroupList: React.FC<Props> = ({ className }) => {
-  return <div className={className}></div>;
+
+export const ProductsGroupList: React.FC<Props> = ({
+  title,
+  items,
+  listClassName,
+  className,
+}) => {
+  return (
+    <div className={className}>
+      <Title text={title} size='lg' className='font-extrabold mb-5' />
+      <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+        {items.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            imageUrl={product.imageUrl}
+            price={product.items[0].price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
