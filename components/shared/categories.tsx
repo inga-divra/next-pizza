6e6200f39a8aa9ef8d902e09f1621bc925efc818
inga-divra <ingady@gmail.com>
2025-02-05 16:51:25 +0200

    Added dynamic category activation on scroll using Zustand
    - Added logic to automatically switch the active category while scrolling.
    - Used useIntersection to track section visibility.
    - Stored the active category state in Zustand to avoid prop drilling.
    - Fixed the initial active category (now starts with Pizza).

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,28 +1,38 @@
+'use client';
+
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '@/store/category';
 import React from 'react';
 
 interface Props {
   className?: string;
 }
 
-const cats = ['Pizzas', 'Combo', 'Cocktails', 'Coffee', 'Drinks', 'Desserts'];
-const activeIndex = 0;
+const cats = [
+  { id: 0, name: 'Pizzas' },
+  { id: 1, name: 'Snacks' },
+  { id: 2, name: 'Desserts' },
+  { id: 3, name: 'Coffee' },
+  { id: 4, name: 'Cocktails' },
+  { id: 5, name: 'Drinks' },
+];
 
 export const Categories: React.FC<Props> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}
     >
-      {cats.map((cat, index) => (
+      {cats.map(({ id, name }, index) => (
         <a
           className={cn(
             'flex items-center font-bold h-11 rounded-2xl px-5',
-            activeIndex === index &&
+            categoryActiveId === id &&
               'bg-white shadow-md shadow-gray-50 text-primary'
           )}
           key={index}
         >
-          <button>{cat}</button>
+          <button>{name}</button>
         </a>
       ))}
     </div>
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -4,6 +4,7 @@ import { Title } from './title';
 import { cn } from '@/lib/utils';
 import { ProductCard } from './product-card';
 import { useIntersection } from 'react-use';
+import { useCategoryStore } from '@/store/category';
 
 interface ProductVariant {
   price: number;
@@ -31,6 +32,7 @@ export const ProductsGroupList: React.FC<Props> = ({
   categoryId,
   className,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
   const intersectionRef = React.useRef(null);
   const intersection = useIntersection(intersectionRef, {
     threshold: 0.4,
@@ -38,7 +40,7 @@ export const ProductsGroupList: React.FC<Props> = ({
 
   useEffect(() => {
     if (intersection?.isIntersecting) {
-      console.log(title, categoryId);
+      setActiveCategoryId(categoryId);
     }
   }, [categoryId, intersection?.isIntersecting, title]);
   return (
