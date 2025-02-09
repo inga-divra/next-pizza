ffd3e1b0304fbfd7ecaab170d9e99024146edf99
inga-divra <ingady@gmail.com>
2025-02-09 18:55:25 +0200

    fixed prisma client

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,13 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-    return new PrismaClient();
-};
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
 }
-
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
