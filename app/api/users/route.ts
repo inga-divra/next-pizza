3177d74072eaf78a7bb83748c84cd8b0f7326b36
inga-divra <ingady@gmail.com>
2025-02-09 19:29:17 +0200

    .

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,16 @@
 import { prisma } from "@/prisma/prisma-client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     const users = await prisma.user.findMany();
     return NextResponse.json(users);
 }
 
+export async function POST(request: NextRequest) {
+    const data = await request.json();
+    console.log('Received data:', data);
+    const user = await prisma.user.create({
+        data
+    });
+    return NextResponse.json(user);
+}
