81297a00e325774c10bb3b7207e6796fb6f63a28
inga-divra <ingady@gmail.com>
2025-03-05 18:31:34 +0200

    fi3

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,38 +1,10 @@
-import { Container, ProductImage, Title } from '@/components/shared';
-import { prisma } from '@/prisma/prisma-client';
-import { notFound } from 'next/navigation';
+'use client';
 
-export default async function ProductPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  const product = await prisma.product.findFirst({
-    where: {
-      id: Number(id),
-    },
-  });
+import { useParams } from 'next/navigation';
 
-  if (!product) return notFound();
+export default function ProductPage() {
+  const params = useParams();
+  const id = params?.id as string;
 
-  return (
-    <Container className='flex flex-col my-10'>
-      <div className='flex flex-1'>
-        <ProductImage imageUrl={product.imageUrl} size={40} />
-
-        <div className='w-[490px] bg-[#f7f6f5] p-7'>
-          <Title
-            text={product.name}
-            size='md'
-            className='font-extrabold mb-1'
-          />
-
-          <p className='text-gray-400'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
-            et!
-          </p>
-        </div>
-      </div>
-    </Container>
-  );
+  return <p>Product {id}</p>;
 }
