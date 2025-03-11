ccdef8c51e4f13125848a143a9dd3f313f599aab
inga-divra <ingady@gmail.com>
2025-03-11 19:29:55 +0200

    Ensure params is awaited

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,42 +1,26 @@
-import { Container, Title, ProductImage } from '@/components/shared';
-import { prisma } from '@/prisma/prisma-client';
 import { notFound } from 'next/navigation';
-
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
+import { prisma } from '@/prisma/prisma-client';
 
 export default async function ProductPage({
-  params: { id },
-}: ProductPageProps) {
+  params,
+}: {
+  params: { id: string };
+}) {
+  // Ensure params is awaited
+  const { id } = await params;
+
+  // Получаем продукт
   const product = await prisma.product.findFirst({
     where: { id: Number(id) },
   });
 
   if (!product) {
-    return notFound();
+    notFound(); // Отображает страницу 404
   }
 
   return (
-    <Container className='flex flex-col my-10'>
-      <div className='flex flex-1'>
-        <ProductImage imageUrl={product.imageUrl} size={40} />
-
-        <div className='w-[490px] bg-[#f7f6f5] p-7'>
-          <Title
-            text={product.name}
-            size='md'
-            className='font-extrabold mb-1'
-          />
-
-          <p className='text-gray-400'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
-            et!
-          </p>
-        </div>
-      </div>
-    </Container>
+    <div>
+      <h1>{product.name}</h1>
+    </div>
   );
 }
